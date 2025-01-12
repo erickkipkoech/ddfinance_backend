d67b4f43b8385e15d89fa011c304c48d63bb8ce3
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2025-01-12 19:40:08 +0300

    first commit

--- obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e0aa2ef682921ae33f20d2a9571e9589da2417")]
 [assembly: System.Reflection.AssemblyProductAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
