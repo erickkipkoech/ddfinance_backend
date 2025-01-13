a27a20d79d450d0191e9a30599f7474bd36aae8a
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2025-01-13 21:54:18 +0300

    add docker compose

--- obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6072835347762e8cb0ee3c122b65813dbd53d1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee8a422c71c135539d51aee94e78513d70dee8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
