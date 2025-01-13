d502c5819f2c650209fc4c84404b852357aaf740
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2025-01-13 22:55:52 +0300

    message

--- obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ddfinance_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d8b55cef43baa6443f948bca2b3654f5e07499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1056c0c2bf3da181d681d3730e3fd45574108172")]
 [assembly: System.Reflection.AssemblyProductAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ddfinance_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
